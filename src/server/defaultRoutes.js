e2f9c3325053b16cd47fa370e42e9c9fa794814c
Carl <carl@carlossus.com>
2015-09-26 18:02:42 +0800

    Add default front-end lib routes

---
+++ src/server/defaultRoutes.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var route = require('../routes/add');
+var root = path.resolve(__dirname, '../../node_modules');
+var bootstrapJs = path.resolve(root, 'boostrap/dist/js/bootstrap.min.js');
+var bootstrapCss = path.resolve(root, 'boostrap/dist/css/bootstrap.min.css');
+var knockout = path.resolve(root, 'knockout/build/output/knockout-latest.js');
+var cajon = path.resolve(root, 'cajon/cajon.js');
+route.get('/scripts/bootstrap.js')(function (request, reply) { return reply.file(bootstrapJs); });
+route.get('/styles/bootstrap.js')(function (request, reply) { return reply.file(bootstrapCss); });
+route.get('/scripts/knockout.js')(function (request, reply) { return reply.file(knockout); });
+route.get('/scripts/cajon.js')(function (request, reply) { return reply.file(cajon); });
+//# sourceMappingURL=defaultRoutes.js.map
---
+++ src/server/defaultRoutes.ts
@@ -0,0 +1,14 @@
+import path = require('path');
+import route = require('../routes/add');
+var root = path.resolve(__dirname, '../../node_modules');
+
+var bootstrapJs = path.resolve(root, 'boostrap/dist/js/bootstrap.min.js');
+var bootstrapCss = path.resolve(root, 'boostrap/dist/css/bootstrap.min.css');
+var knockout = path.resolve(root, 'knockout/build/output/knockout-latest.js');
+var cajon = path.resolve(root, 'cajon/cajon.js');
+
+route.get('/scripts/bootstrap.js')((request, reply) => reply.file(bootstrapJs));
+route.get('/styles/bootstrap.js')((request, reply) => reply.file(bootstrapCss));
+route.get('/scripts/knockout.js')((request, reply) => reply.file(knockout));
+route.get('/scripts/cajon.js')((request, reply) => reply.file(cajon));
+
