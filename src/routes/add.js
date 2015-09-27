5ec3b0c941e716694929275c37b163f10b75bbf7
Carl <carl@carlossus.com>
2015-09-27 12:04:28 +0800

    Re-build js

--- src/routes/add.js
+++ src/routes/add.js
@@ -1,42 +1,34 @@
 var server_1 = require('../server');
-function get(route) {
-    return function (handler) {
-        server_1.server.route({
-            method: 'GET',
-            path: route,
-            handler: handler
-        });
-    };
+function get(route, handler) {
+    server_1.server.route({
+        method: 'GET',
+        path: route,
+        handler: handler
+    });
 }
 exports.get = get;
-function post(route) {
-    return function (handler) {
-        server_1.server.route({
-            method: 'POST',
-            path: route,
-            handler: handler
-        });
-    };
+function post(route, handler) {
+    server_1.server.route({
+        method: 'POST',
+        path: route,
+        handler: handler
+    });
 }
 exports.post = post;
-function put(route) {
-    return function (handler) {
-        server_1.server.route({
-            method: 'PUT',
-            path: route,
-            handler: handler
-        });
-    };
+function put(route, handler) {
+    server_1.server.route({
+        method: 'PUT',
+        path: route,
+        handler: handler
+    });
 }
 exports.put = put;
-function del(route) {
-    return function (handler) {
-        server_1.server.route({
-            method: 'DELETE',
-            path: route,
-            handler: handler
-        });
-    };
+function del(route, handler) {
+    server_1.server.route({
+        method: 'DELETE',
+        path: route,
+        handler: handler
+    });
 }
 exports.del = del;
 //# sourceMappingURL=add.js.map
