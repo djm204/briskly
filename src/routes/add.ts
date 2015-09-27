963be1528d314467e20d6bd5080b4157f64f7007
Carl <carl@carlossus.com>
2015-09-27 12:03:40 +0800

    Change function signature off add routes

--- src/routes/add.ts
+++ src/routes/add.ts
@@ -1,42 +1,34 @@
 import Hapi = require('hapi');
 import { server } from '../server';
 
-export function get(route: string) {
-    return (handler: Hapi.ISessionHandler | Hapi.IRouteHandlerConfig) => {
-        server.route({
-            method: 'GET',
-            path: route,
-            handler
-        });
-    }
+export function get(route: string, handler: Hapi.ISessionHandler | Hapi.IRouteFailFunction) {
+    server.route({
+        method: 'GET',
+        path: route,
+        handler
+    });
 }
 
-export function post(route: string) {
-    return (handler: Hapi.ISessionHandler | Hapi.IRouteHandlerConfig) => {
-        server.route({
-            method: 'POST',
-            path: route,
-            handler
-        });
-    }
+export function post(route: string, handler: Hapi.ISessionHandler | Hapi.IRouteFailFunction) {
+    server.route({
+        method: 'POST',
+        path: route,
+        handler
+    });
 }
 
-export function put(route: string) {
-    return (handler: Hapi.ISessionHandler | Hapi.IRouteHandlerConfig) => {
-        server.route({
-            method: 'PUT',
-            path: route,
-            handler
-        });
-    }
+export function put(route: string, handler: Hapi.ISessionHandler | Hapi.IRouteFailFunction) {
+    server.route({
+        method: 'PUT',
+        path: route,
+        handler
+    });
 }
 
-export function del(route: string) {
-    return (handler: Hapi.ISessionHandler | Hapi.IRouteHandlerConfig) => {
-        server.route({
-            method: 'DELETE',
-            path: route,
-            handler
-        });
-    }
+export function del(route: string, handler: Hapi.ISessionHandler | Hapi.IRouteFailFunction) {
+    server.route({
+        method: 'DELETE',
+        path: route,
+        handler
+    });
 }
