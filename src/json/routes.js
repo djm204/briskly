69b545d11deb45bc6fed83e17eeaf7ff98ec463e
Carl <carl@carlossus.com>
2015-10-01 21:35:29 +0800

    Implement briskly.json route parsing

--- src/json/routes.js
+++ src/json/routes.js
@@ -1,7 +1,9 @@
+var Types = require('../../index.d.ts');
 var json = require('./read');
 var log = require('ls-logger');
 var server_1 = require('../server');
 var path = require('path');
+var fs = require('fs');
 var workingDirectory = process.env.PWD;
 function parseRoutes() {
     if (!json.routes) {
@@ -10,6 +12,7 @@ function parseRoutes() {
     }
     for (var routePath in json.routes) {
         var route = json.routes[routePath];
+        route.path = routePath;
         if (!route.method) {
             log.warn(routePath + ": No method found. Route ignored.");
             continue;
@@ -18,31 +21,113 @@ function parseRoutes() {
             log.warn(routePath + ": No handler found. Route ignored.");
             continue;
         }
-        var handlerPath = path.join(workingDirectory, route.handler);
-        switch (getExtension(routePath)) {
-            case '.js': {
-                var handler = require(handlerPath);
-                if (typeof handler !== 'function') {
-                    log.warn(routePath + ": Handler is not a function, but .js provided. Route ignored.");
-                    break;
-                }
-                server_1.server.route({
-                    method: route.method,
-                    path: routePath,
-                    handler: handler
-                });
-                log.info(route.method.toLocaleUpperCase() + " " + routePath);
+        switch (getHandlerType(routePath)) {
+            case 0 /* Function */:
+                addFunctionRoute(route);
                 break;
-            }
+            case 2 /* File */:
+                addFileRoute(route);
+                break;
+            case 1 /* Directory */:
+                addDirectoryRoute(route);
+                break;
             default: {
+                log.error(routePath + ": Unable to determine route handler type. Route ignored.");
                 // TODO
                 break;
             }
         }
     }
 }
+function addFunctionRoute(route) {
+    if (!isValidFunctionRoute(route))
+        return;
+    var method = route.method.toUpperCase();
+    try {
+        /**
+         * This allows us to write in-line functions in briskly.json
+         * Example:
+         * ...
+         * handler: function(request, reply) { reply(request.payload.left + request.payload.right); }
+         *
+         * However, this is fairly insane.
+         * */
+        var evalFunc = eval("(" + route.handler + ")");
+        if (typeof evalFunc === 'function') {
+            server_1.server.route({
+                method: method,
+                path: route.path,
+                handler: evalFunc
+            });
+            log.info(route.path + ": Route added");
+            return;
+        }
+    }
+    catch (ex) { } // Couldn't eval the function. We're not too fussed here. It was worth a shot.      
+    try {
+        var routeHandler = require(handlerPath(route));
+        server_1.server.route({
+            method: method,
+            path: route.path,
+            handler: routeHandler
+        });
+        log.info(route.path + ": Route added");
+    }
+    catch (ex) {
+        log.error(route.path + ": Unable to load function route handler. Route ignored.");
+    }
+}
+function addFileRoute(route) {
+    var handlerPath = handlerPath(route.handler);
+    server_1.server.route({
+        method: route.method.toUpperCase(),
+        path: route.path,
+        handler: function (request, reply) { return reply.file(handlerPath); }
+    });
+}
+function addDirectoryRoute(route) {
+    var handlerPath = handlerPath(route.handler);
+    server_1.server.route({
+        method: route.method.toUpperCase(),
+        path: route.path + '/{param*}',
+        handler: {
+            directory: {
+                path: handlerPath
+            }
+        }
+    });
+}
+function handlerPath(route) {
+    var handlerPath = path.resolve(path.join(workingDirectory, route.handler));
+    return handlerPath;
+}
+function isValidFunctionRoute(route) {
+    var routeHandler = require(handlerPath(route));
+    if (typeof routeHandler !== 'function') {
+        log.warn(route.path + ": Handler is not a function. Route ignored.");
+        return false;
+    }
+    return true;
+}
 function getExtension(routePath) {
     var ext = path.extname(routePath) || '.js';
     return ext;
+}
+function getHandlerType(handler) {
+    var handlerPath = handlerPath(handler);
+    var extension = getExtension(handler);
+    if (extension === '.js' || extension === '')
+        return 0 /* Function */;
+    try {
+        var stat = fs.statSync(handlerPath);
+        if (stat.isFile())
+            return 2 /* File */;
+        if (stat.isDirectory())
+            return 1 /* Directory */;
+        return 4 /* Unknown */;
+    }
+    catch (ex) {
+        return 3 /* NotFound */;
+    }
 }
 //# sourceMappingURL=routes.js.map
--- src/json/routes.ts
+++ src/json/routes.ts
@@ -1,8 +1,12 @@
-import Types = require('../../index.d.ts')
+import Types = require('../../index.d.ts');
+import Handler = Types.Handler;
+import Route = Types.Route;
+
 import json = require('./read');
 import log = require('ls-logger');
 import { server } from '../server';
 import path = require('path');
+import fs = require('fs');
 
 var workingDirectory = process.env.PWD;
 
@@ -14,6 +18,8 @@ function parseRoutes() {
 
     for (var routePath in json.routes) {
         var route: Types.Route = json.routes[routePath];
+        route.path = routePath;
+
         if (!route.method) {
             log.warn(`${routePath}: No method found. Route ignored.`);
             continue;
@@ -23,25 +29,22 @@ function parseRoutes() {
             log.warn(`${routePath}: No handler found. Route ignored.`);
             continue;
         }
-        var handlerPath = path.join(workingDirectory, route.handler);
-        switch (getExtension(routePath)) {
-            case '.js': {
-                var handler = require(handlerPath);
-                if (typeof handler !== 'function') {
-                    log.warn(`${routePath}: Handler is not a function, but .js provided. Route ignored.`);
-                    break;
-                }
 
-                server.route({
-                    method: route.method,
-                    path: routePath,
-                    handler
-                });
-
-                log.info(`${route.method.toLocaleUpperCase() } ${routePath}`);
+        switch (getHandlerType(routePath)) {
+            case Handler.Function:
+                addFunctionRoute(route);
                 break;
-            }
+                
+            case Handler.File:
+                addFileRoute(route);
+                break;
+                
+            case Handler.Directory:
+                addDirectoryRoute(route);
+                break;
+                
             default: {
+                log.error(`${routePath}: Unable to determine route handler type. Route ignored.`);
                 // TODO
                 break;
             }
@@ -49,8 +52,107 @@ function parseRoutes() {
     }
 }
 
+function addFunctionRoute(route: Route) {
+    if (!isValidFunctionRoute(route)) return;
+    var method = route.method.toUpperCase();
+
+    try {
+        /** 
+         * This allows us to write in-line functions in briskly.json
+         * Example:
+         * ...
+         * handler: function(request, reply) { reply(request.payload.left + request.payload.right); }
+         * 
+         * However, this is fairly insane.
+         * */ 
+        var evalFunc = eval(`(${route.handler})`);
+        if (typeof evalFunc === 'function') {
+            server.route({
+                method,
+                path: route.path,
+                handler: evalFunc
+            });
+            log.info(`${route.path}: Route added`);
+            return;
+        }
+    }
+    catch (ex) { } // Couldn't eval the function. We're not too fussed here. It was worth a shot.      
+               
+    try {
+        var routeHandler = require(handlerPath(route));
+
+        server.route({
+            method,
+            path: route.path,
+            handler: routeHandler
+        });
+        log.info(`${route.path}: Route added`);
+    }
+    catch (ex) {
+        log.error(`${route.path}: Unable to load function route handler. Route ignored.`);
+    }
+}
+
+function addFileRoute(route: Route) {
+    var handlerPath = handlerPath(route.handler);
+    
+    server.route({
+        method: route.method.toUpperCase(),
+        path: route.path,
+        handler: (request, reply) => reply.file(handlerPath)
+    });    
+}
+
+function addDirectoryRoute(route: Route) {
+    var handlerPath = handlerPath(route.handler);
+    
+    server.route({
+        method: route.method.toUpperCase(),
+        path: route.path + '/{param*}',
+        handler: {
+            directory: {
+                path: handlerPath
+            }
+        } 
+    })
+}
+
+function handlerPath(route: Route) {
+    var handlerPath = path.resolve(path.join(workingDirectory, route.handler));
+    return handlerPath;
+}
+
+function isValidFunctionRoute(route: Route) {
+    var routeHandler = require(handlerPath(route));
+    if (typeof routeHandler !== 'function') {
+        log.warn(`${route.path}: Handler is not a function. Route ignored.`);
+        return false;
+    }
+
+    return true;
+}
+
 function getExtension(routePath: string) {
     var ext = path.extname(routePath) || '.js';
 
     return ext;
 }
+
+function getHandlerType(handler: string): Handler {
+    var handlerPath = handlerPath(handler);
+
+    var extension = getExtension(handler);
+    if (extension === '.js' || extension === '')
+        return Handler.Function;
+
+    try {
+        var stat = fs.statSync(handlerPath);
+
+        if (stat.isFile()) return Handler.File;
+        if (stat.isDirectory()) return Handler.Directory;
+
+        return Types.Handler.Unknown;
+    } catch (ex) {
+        return Types.Handler.NotFound;
+    }
+} 
