db5697bd0f9b31a6637d453fd9f178bf8a9c0e1b
Carl <carl@carlossus.com>
2015-10-01 21:46:56 +0800

    Propagate basic exported api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-require('./json/read');
+var routeLoader = require('./json/routes');
+var web = require('./server');
+var log = require('ls-logger');
+function init() {
+    // Initial parsing of briskly.json
+    var json = require('./json/read');
+    // Parse briskly.json routes
+    routeLoader();
+    // Start the web server
+    web.start()
+        .then(function () { return log.info('Web server started'); });
+}
+module.exports = init;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-require('./json/read');
+import routeLoader = require('./json/routes');
+import * as web from './server';
+import log = require('ls-logger');
+export = init;
+
+function init() {
+    // Initial parsing of briskly.json
+    var json = require('./json/read');
+    
+    // Parse briskly.json routes
+    routeLoader();
+    
+    // Start the web server
+    web.start()
+        .then(() => log.info('Web server started'));
+}
