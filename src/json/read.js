451bbb90a625770a4a76f6de52ab4c9f17c63caf
Carl <carl@carlossus.com>
2015-10-02 23:02:09 +0800

    Add 'includes' functionality to json parser

--- src/json/read.js
+++ src/json/read.js
@@ -1,13 +1,47 @@
 var fs = require('fs');
 var path = require('path');
 var logger = require('ls-logger');
+var workingDirectory = process.env.PWD;
 var jsonPath = path.resolve(process.env.PWD, 'briskly.json');
 var brisklyJson;
 try {
     brisklyJson = JSON.parse(fs.readFileSync(jsonPath).toString());
+    shimIncludes();
 }
 catch (ex) {
     logger.error("Failed to read brisky.json: " + ex.message);
+}
+function shimIncludes() {
+    if (!brisklyJson.routes && brisklyJson.routes['include'])
+        return;
+    var include = brisklyJson.routes['include'];
+    if (!include['files'])
+        return;
+    var files = include['files'];
+    if (!Array.isArray(files))
+        return;
+    var includePath = include['root'] || '/';
+    var basePath = path.resolve(path.join(workingDirectory, includePath));
+    var addRoute = function (routePath, route) {
+        if (brisklyJson.routes[route.path]) {
+            logger.warn(routePath + ": Duplicate route. Route ignored.");
+            return;
+        }
+        route.path = routePath;
+        brisklyJson.routes[routePath] = route;
+    };
+    files.forEach(function (file) {
+        var filePath = path.resolve(path.join(basePath, file));
+        try {
+            var includeBody = fs.readFileSync(filePath).toString();
+            var json = JSON.parse(includeBody);
+            Object.keys(json)
+                .forEach(function (key) { return addRoute(key, json[key]); });
+        }
+        catch (ex) {
+            logger.error(file + ": Unable to parse include: " + ex.message);
+        }
+    });
 }
 module.exports = brisklyJson;
 //# sourceMappingURL=read.js.map
--- src/json/read.ts
+++ src/json/read.ts
@@ -4,12 +4,53 @@ import path = require('path');
 import logger = require('ls-logger');
 export = brisklyJson;
 
+var workingDirectory = process.env.PWD;
 var jsonPath = path.resolve(process.env.PWD, 'briskly.json');
 var brisklyJson: Types.Config;
 
 try {
     brisklyJson = JSON.parse(fs.readFileSync(jsonPath).toString());
+    shimIncludes();
 }
 catch (ex) {
     logger.error(`Failed to read brisky.json: ${ex.message}`);
+}
+
+function shimIncludes() {
+
+    if (!brisklyJson.routes && brisklyJson.routes['include'])
+        return;
+
+    var include = brisklyJson.routes['include'];
+
+    if (!include['files']) return;
+
+    var files: string[] = include['files'];
+    if (!Array.isArray(files)) return;
+
+    var includePath = include['root'] || '/';
+    var basePath = path.resolve(path.join(workingDirectory, includePath));
+
+    var addRoute = (routePath: string, route: Types.Route) => {
+        if (brisklyJson.routes[route.path]) {
+            logger.warn(`${routePath}: Duplicate route. Route ignored.`);
+            return;
+        }
+
+        route.path = routePath;
+        brisklyJson.routes[routePath] = route;
+    }
+
+    files.forEach(file => {
+        var filePath = path.resolve(path.join(basePath, file));
+        try {
+            var includeBody = fs.readFileSync(filePath).toString();
+            var json = JSON.parse(includeBody);
+            Object.keys(json)
+                .forEach(key => addRoute(key, json[key]))
+        }
+        catch (ex) {
+            logger.error(`${file}: Unable to parse include: ${ex.message}`);
+        }
+    })
 }
