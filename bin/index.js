80e0650f76a43702605037ee6601492cc188d899
Carl <carl@carlossus.com>
2015-11-20 19:09:44 +0800

    Correctly detect CLI arguments

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
-
+var log = require('ls-logger');
 var start = require('./start');
 var init = require('./init');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
-if (args['start']) {
-    start.default();
-}
-if (args['run']) {
-    start.default();
-}
-if (args['init']) {
-    init.default();
+
+var command = args._[0].toLowerCase();
+run();
+
+function run() {
+    switch (command) {
+        case 'start':
+            return start.default();
+        case 'run':
+            return start.default();
+        case 'init':
+            return init.default();
+    }
+    log.error('Command not found: ' + command);
 }
