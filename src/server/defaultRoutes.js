c3d0b5bced2d9138af168fe8ccae3ee51dcace48
Carl <carl@carlossus.com>
2015-11-21 13:00:55 +0800

    Add briskly bootstrapper routes and scripts to default routes

--- src/server/defaultRoutes.js
+++ src/server/defaultRoutes.js
@@ -13,13 +13,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, Promi
 };
 var path = require('path');
 var route = require('../routes/add');
+var bootstrap_1 = require('../bootstrap');
 var root = path.resolve(__dirname, '../../node_modules');
+var brisklyRoot = path.resolve(__dirname, '../../');
 var bootstrapJs = path.resolve(root, 'boostrap/dist/js/bootstrap.min.js');
 var bootstrapCss = path.resolve(root, 'boostrap/dist/css/bootstrap.min.css');
 var knockout = path.resolve(root, 'knockout/build/output/knockout-latest.js');
 var cajon = path.resolve(root, 'cajon/cajon.js');
-route.get('/scripts/bootstrap.js', (request, reply) => reply.file(bootstrapJs));
-route.get('/styles/bootstrap.css', (request, reply) => reply.file(bootstrapCss));
-route.get('/scripts/knockout.js', (request, reply) => reply.file(knockout));
-route.get('/scripts/cajon.js', (request, reply) => reply.file(cajon));
+var briskly = path.resolve(brisklyRoot, 'src/bootstrap/briskly.js');
+function loadRoutes() {
+    route.get('/scripts/bootstrap.js', (request, reply) => reply.file(bootstrapJs));
+    route.get('/styles/bootstrap.css', (request, reply) => reply.file(bootstrapCss));
+    route.get('/scripts/knockout.js', (request, reply) => reply.file(knockout));
+    route.get('/scripts/cajon.js', (request, reply) => reply.file(cajon));
+    route.get('/scripts/briskly.js', (request, reply) => reply.file(briskly));
+    bootstrap_1.default();
+}
+exports.default = loadRoutes;
 //# sourceMappingURL=defaultRoutes.js.map
--- src/server/defaultRoutes.ts
+++ src/server/defaultRoutes.ts
@@ -1,14 +1,24 @@
 import * as path from 'path'
-import route = require('../routes/add');
+import * as route from '../routes/add';
+import brisklyBootstrapper from '../bootstrap';
+export { loadRoutes as default }
+
 var root = path.resolve(__dirname, '../../node_modules');
+var brisklyRoot = path.resolve(__dirname, '../../');
+
 
 var bootstrapJs = path.resolve(root, 'boostrap/dist/js/bootstrap.min.js');
 var bootstrapCss = path.resolve(root, 'boostrap/dist/css/bootstrap.min.css');
 var knockout = path.resolve(root, 'knockout/build/output/knockout-latest.js');
 var cajon = path.resolve(root, 'cajon/cajon.js');
+var briskly = path.resolve(brisklyRoot, 'src/bootstrap/briskly.js');
 
-route.get('/scripts/bootstrap.js', (request, reply) => reply.file(bootstrapJs));
-route.get('/styles/bootstrap.css', (request, reply) => reply.file(bootstrapCss));
-route.get('/scripts/knockout.js', (request, reply) => reply.file(knockout));
-route.get('/scripts/cajon.js', (request, reply) => reply.file(cajon));
+function loadRoutes() {
+    route.get('/scripts/bootstrap.js', (request, reply) => reply.file(bootstrapJs));
+    route.get('/styles/bootstrap.css', (request, reply) => reply.file(bootstrapCss));
+    route.get('/scripts/knockout.js', (request, reply) => reply.file(knockout));
+    route.get('/scripts/cajon.js', (request, reply) => reply.file(cajon));
+    route.get('/scripts/briskly.js', (request, reply) => reply.file(briskly));
 
+    brisklyBootstrapper();    
+}
