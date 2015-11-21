fa6b17089b49dae18c262456bf5678edeba60e90
Carl <carl@carlossus.com>
2015-11-21 13:01:17 +0800

    Create test 'main' html and add to briskly.json

--- dist/server/defaultRoutes.js
+++ dist/server/defaultRoutes.js
@@ -31,21 +31,31 @@ var __awaiter = undefined && undefined.__awaiter || function (thisArg, _argument
 };
 var path = require('path');
 var route = require('../routes/add');
+var bootstrap_1 = require('../bootstrap');
 var root = path.resolve(__dirname, '../../node_modules');
+var brisklyRoot = path.resolve(__dirname, '../../');
 var bootstrapJs = path.resolve(root, 'boostrap/dist/js/bootstrap.min.js');
 var bootstrapCss = path.resolve(root, 'boostrap/dist/css/bootstrap.min.css');
 var knockout = path.resolve(root, 'knockout/build/output/knockout-latest.js');
 var cajon = path.resolve(root, 'cajon/cajon.js');
-route.get('/scripts/bootstrap.js', function (request, reply) {
-    return reply.file(bootstrapJs);
-});
-route.get('/styles/bootstrap.css', function (request, reply) {
-    return reply.file(bootstrapCss);
-});
-route.get('/scripts/knockout.js', function (request, reply) {
-    return reply.file(knockout);
-});
-route.get('/scripts/cajon.js', function (request, reply) {
-    return reply.file(cajon);
-});
+var briskly = path.resolve(brisklyRoot, 'src/bootstrap/briskly.js');
+function loadRoutes() {
+    route.get('/scripts/bootstrap.js', function (request, reply) {
+        return reply.file(bootstrapJs);
+    });
+    route.get('/styles/bootstrap.css', function (request, reply) {
+        return reply.file(bootstrapCss);
+    });
+    route.get('/scripts/knockout.js', function (request, reply) {
+        return reply.file(knockout);
+    });
+    route.get('/scripts/cajon.js', function (request, reply) {
+        return reply.file(cajon);
+    });
+    route.get('/scripts/briskly.js', function (request, reply) {
+        return reply.file(briskly);
+    });
+    bootstrap_1.default();
+}
+exports.default = loadRoutes;
 //# sourceMappingURL=defaultRoutes.js.map
