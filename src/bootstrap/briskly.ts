531603d19fa64ccda2d7c77d4d0239a36a2290c3
Carl Winkler <carl@carlossus.com>
2015-11-25 12:42:29 +0800

    WIP: retrieve template and viewmodel retrieval by naming convention

--- src/bootstrap/briskly.ts
+++ src/bootstrap/briskly.ts
@@ -9,8 +9,15 @@
 
 (<any>require)(['knockout', 'jquery'], (ko: KnockoutStatic, $: JQueryStatic) => {
 	var customerLoader = {
-		loadTemplate: (name: string, config: any, callback: any) => {
+		getConfig: (name: string, callback: (component: { viewModel: any, template: any }) => void) => {
+			// Get the template markup
+		 	var template = $.get(`/components/${name}.html`);
+			 
+			// Get the viewModel constructor
+			var viewModel = (<any>require)(`../components/${name}`);
 			
+			
+			callback({ viewModel: null, template: null });
 		}
 	};
 
